95952b2cbfeea3ee1a1b650a47ec04e51586ca91
Mitchell Swindell <mitchellswindell@Mitchells-MacBook-Pro-6.local>
2023-01-26 19:36:29 -0500

    fixed small error

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const http = require("http");
 // Require fs module
 const fs = require("fs");
 // Require minimist module (make sure you install this one via npm).
-const minimist = require("minimist");
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const args = (process.argv.slice(2));
+const args = require("minimist")(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args["port"] || 3000;
@@ -17,7 +16,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
     if (err) {
-    console.error(err);
+    console.log("error");
     return;
     }
 // Define a const `server` as an arrow function using http.createServer. 
